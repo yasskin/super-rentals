a33c7411ce31a980cfe37d4ae26967efe04e797f
Noah Yasskin and Tony Fabery <noah@>
2016-08-15 16:42:48 -0700

    add array of announcements to the component

--- app/routes/announcements.js
+++ app/routes/announcements.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
+var announcements = [{
+  id: 1,
+  title: "Rapper ShadowDom in shootout with Stormzy",
+  description: "bullet holes in Stormzy's crib"
+}, {
+  id: 2,
+  title: "Tonyzz eats Sammy's sandwich",
+  description: "too lurid to print"
+}, {
+  id: 3,
+  title: "Shit gets wack @ Epicodus",
+  description: "Floor 8 hoop destroyed"
+}];
+
 export default Ember.Route.extend({
+  model() {
+    return announcements;
+  },
 });
