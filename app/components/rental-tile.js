01c4905948792ce43d2c744fb65d4fdac8f824bf
Noah Yasskin and Tony Fabery <noah+tony@>
2016-08-15 15:30:26 -0700

    add application template

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -9,14 +9,6 @@ export default Ember.Component.extend({
     },
     imageHide() {
       this.set('isImageShowing', false);
-    },
-    update(rental, params) {
-      this.sendAction('update', rental, params);
-    },
-    delete(rental) {
-      if (confirm('Untermensh - Rahee, this basically means a moron, are you sure you want to destroy this rental?')) {
-        this.sendAction('destroyRental', rental);
-      }
     }
   }
 });
