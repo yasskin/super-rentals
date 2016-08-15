9ab44c2de4b1b0c7444d5042b5a49e8c269acaf3
Noah Yasskin and Tony Fabery <noah+tony@>
2016-08-15 14:50:46 -0700

    add rental form and sending up the things from here with the send action its sending these two arguments up for the update function or the update method you don't need to add all thsoe just put update rentals raheem cant understand these commits

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -2,15 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isImageShowing: false,
+  updateRentalForm: false,
   actions: {
-    imageShow: function() {
+    imageShow() {
       this.set('isImageShowing', true);
     },
-    imageHide: function() {
+    imageHide() {
       this.set('isImageShowing', false);
     },
+    update(rental, params) {
+      this.sendAction('update', rental, params);
+    },
     delete(rental) {
-      if (confirm('Untermensh - moron, are you sure you want to destroy this rental?')) {
+      if (confirm('Untermensh - Rahee, this basically means a moron, are you sure you want to destroy this rental?')) {
         this.sendAction('destroyRental', rental);
       }
     }
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('rental');
+    return Ember.RSVP.hash({
+      cities: this.store.findAll('city'),
+      rentals: this.store.findAll('rental')
+    });
   },
 
   actions: {
@@ -11,7 +14,17 @@ export default Ember.Route.extend({
       newRental.save();
       this.transitionTo('index');
     },
-    
+
+    update(rental, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          rental.set(key,params[key]);
+        }
+      });
+      rental.save();
+      this.transitionTo('index');
+    },
+
     destroyRental(rental) {
       rental.destroyRecord();
       this.transitionTo('index');
