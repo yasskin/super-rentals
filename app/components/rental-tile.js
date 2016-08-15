ea7adcf9c3d185b8dda22ed62d95175ed7042ddd
Noah Yasskin and Tony Fabery <tony@>
2016-08-15 11:25:42 -0700

    add create and delete function

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isImageShowing: false,
+  actions: {
+    imageShow: function() {
+      this.set('isImageShowing', true);
+    },
+    imageHide: function() {
+      this.set('isImageShowing', false);
+    },
+    delete(rental) {
+      if (confirm('Untermensh - moron, are you sure you want to destroy this rental?')) {
+        this.sendAction('destroyRental', rental);
+      }
+    }
+  }
 });
