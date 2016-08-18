49974adb2f0ff7cce40dc558b7f2095c81bb3180
Noah Yasskin and Rony Nasr <noah@>
2016-08-18 09:35:23 -0700

    add new city component

--- app/components/city-tile.js
+++ app/components/city-tile.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  
+  actions: {
+    delete(city){
+      if(confirm("Are you sure you want to remove this city?")){
+        this.sendAction('destroyCity', city);
+      }
+    }
+  }
 });
--- app/routes/city.js
+++ app/routes/city.js
@@ -5,14 +5,16 @@ export default Ember.Route.extend({
     return this.store.findRecord('city', params.city_id);
   },
   actions: {
-    save3(params) {
-      var newRental = this.store.createRecord('rental', params);
-      var city = params.city;
-      city.get('rentals').addObject(newRental);
-      newRental.save().then(function() {
-        return city.save();
+
+    destroyCity(city) {
+      var rental_deletions = city.get('rentals').map(function(rental) {
+        return rental.destroyRecord();
       });
-      this.transitionTo('city', params.city);
-    }    
+      Ember.RSVP.all(rental_deletions).then(function() {
+        return city.destroyRecord();
+      });
+      this.transitionTo('index');
+    }
+
   }
 });
